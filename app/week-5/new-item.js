0435b741fac02b129b091203097ecadf2c44355c
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-10-11 00:16:20 -0600

    Fixed small issue with identation to make the code more readable

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -54,44 +54,43 @@ export default function NewItem()
         setName("");
         setQuantity(1);
     setCategory("produce");
-}
+    }
 
 
-return (
-    <div className="rounded-lg border border-gray-300 bg-white p-4 shadow-sm">
-            <form onSubmit={handleSubmit} className="mb-4">
-                <label className="mb-2 block font-semibold text-gray-700" htmlFor="name">Item Name:</label>
-                <input
-                    type="text"
-                    required
-                    placeholder = "e.g., milk, 4 L 🥛"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)} //Arrow function to update name state on input change
-                    className="mb-3 block w-full rounded-md border-2 border-black p-2 placeholder-gray-300 text-black mb-8"
-                />
-                <p className="mb-3 text-xl font-semibold text-gray-800">
-                    Quantity: <span className="text-green-600">{quantity}</span>
-                </p>
-                <div className="flex items-center gap-3">
-                    <button type="button" className="rounded-md px-4 py-2 font-bold text-black bg-gray-300 hover:bg-gray-300 disabled:bg-gray-100 disabled:text-gray-400" disabled={quantity === 1} onClick={decrement}>-</button>
-                    <button type="button"className="rounded-md px-4 py-2 font-bold text-white bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300" disabled={quantity === 20} onClick={increment}>+</button>
-                </div>
-                <p className="mt-2 text-xs text-gray-600">Allowed range 1-20</p>
-                <label className="mb-2 mt-4 block font-semibold text-gray-700" htmlFor="category">Category:</label>
-                <select
-                    id="category"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)} //Arrow function to update category state on selection change
-                    className="mb-3 block w-full rounded-md border-2 border-black p-2 text-black mb-2"
-                >
-                    {categories.map((category) => ( //Neat trick using map to create the options dynamically instead of hardcoding them
-                        <option key={category.value} value={category.value}>{category.label}</option>
-                    ))}
-                </select>
-                <button type="submit" className="mt-4 w-full rounded-md bg-green-600 px-4 py-2 font-bold text-white hover:bg-green-700">Add Item</button>
-            </form>
-        </div>
-    );
-
+    return (
+        <div className="rounded-lg border border-gray-300 bg-white p-4 shadow-sm">
+                <form onSubmit={handleSubmit} className="mb-4">
+                    <label className="mb-2 block font-semibold text-gray-700" htmlFor="name">Item Name:</label>
+                    <input
+                        type="text"
+                        required
+                        placeholder = "e.g., milk, 4 L 🥛"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} //Arrow function to update name state on input change
+                        className="mb-3 block w-full rounded-md border-2 border-black p-2 placeholder-gray-300 text-black mb-8"
+                    />
+                    <p className="mb-3 text-xl font-semibold text-gray-800">
+                        Quantity: <span className="text-green-600">{quantity}</span>
+                    </p>
+                    <div className="flex items-center gap-3">
+                        <button type="button" className="rounded-md px-4 py-2 font-bold text-black bg-gray-300 hover:bg-gray-300 disabled:bg-gray-100 disabled:text-gray-400" disabled={quantity === 1} onClick={decrement}>-</button>
+                        <button type="button"className="rounded-md px-4 py-2 font-bold text-white bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300" disabled={quantity === 20} onClick={increment}>+</button>
+                    </div>
+                    <p className="mt-2 text-xs text-gray-600">Allowed range 1-20</p>
+                    <label className="mb-2 mt-4 block font-semibold text-gray-700" htmlFor="category">Category:</label>
+                    <select
+                        id="category"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)} //Arrow function to update category state on selection change
+                        className="mb-3 block w-full rounded-md border-2 border-black p-2 text-black mb-2"
+                    >
+                        {categories.map((category) => ( //Neat trick using map to create the options dynamically instead of hardcoding them
+                            <option key={category.value} value={category.value}>{category.label}</option>
+                        ))}
+                    </select>
+                    <button type="submit" className="mt-4 w-full rounded-md bg-green-600 px-4 py-2 font-bold text-white hover:bg-green-700">Add Item</button>
+                </form>
+            </div>
+        );
 }
