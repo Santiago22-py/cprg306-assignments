634b27e8fcf31bfc2e9b60701864165038011c29
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-10-28 20:17:49 -0600

    Added week 8 folder along with it's components

---
+++ app/week-8/page.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from 'react';
+import ShoppingListItem from './item-list';
+import NewItem from './new-item';
+
+//Load the data from the JSON file
+import itemsData from "./item.json";
+
+
+
+
+export default function Page() {
+
+  // Initialize state with the imported JSON data
+  const [items, setItems] = useState(itemsData);
+
+  //Event handler for adding an item
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]); //Passes the old items and adds the new item to the list
+  }
+
+  return (
+    <main className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ShoppingListItem items={items} />
+    </main>
+  );
+}
