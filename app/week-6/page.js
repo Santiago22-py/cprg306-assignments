b921d13ffd18900a22318cf98cfd032ebeac636d
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-11-08 11:38:51 -0700

    Changes based on week 5 feedback (Used prettier)

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,10 +1,10 @@
-import ShoppingListItem from './item-list';
+import ShoppingListItem from "./item-list";
 
 export default function Page() {
   return (
     <main className="p-4 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
-      <ShoppingListItem/>
+      <ShoppingListItem />
     </main>
   );
 }
