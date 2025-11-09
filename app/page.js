15a53afc5b86fa651eb2d6bcfbd3c907fdf1842d
Santiago22-py <joaovitor.santiagovianamelo@edu.sait.ca>
2025-11-08 22:18:32 -0700

    Added reference to week 9

--- app/page.js
+++ app/page.js
@@ -18,6 +18,8 @@ export default function Home() {
         <a href="/week-7"> week-7 </a>
         →
         <a href="/week-8"> week-8 </a>
+        →
+        <a href="/week-9"> week-9 </a>
       </p>
     </main>
   );
